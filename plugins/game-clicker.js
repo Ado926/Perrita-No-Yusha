a1043f759ac585e69f5ba0e0706661b0c2e5a051
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-04-17 13:12:19 -0500

    SoyMaycol

--- plugins/game-clicker.js
+++ plugins/game-clicker.js
@@ -1,24 +1,27 @@
-var handler = async (m, { conn, command }) => {
+var handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
-
     if (!user.clicks) user.clicks = 0;
-
     user.clicks += 1;
 
-    let response = `
-*Clicker Game!*
-Has hecho *${user.clicks}* clicks!
-
-Sigue haciendo clicks para ganar puntos y desbloquear sorpresas.
+    let txt = `
+*¡Bienvenido al ClickerBot!*
+Has hecho *${user.clicks}* clicks.
+Sigue haciendo clicks para mejorar tus estadísticas.
     `;
 
-    await conn.sendButton(
-        m.chat,
-        response,
-        'ClickerBot!',
-        [['🔥 ¡Click!', `/${command}`]],
-        m
-    );
+    const buttons = [
+        { buttonId: '.clicker', buttonText: { displayText: '🔥 ¡Click!' }, type: 1 },
+        { buttonId: '.perfil', buttonText: { displayText: '📊 Ver Perfil' }, type: 1 }
+    ];
+
+    const buttonMessage = {
+        text: txt.trim(),
+        footer: 'ClickerBot',
+        buttons: buttons,
+        headerType: 1
+    };
+
+    await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
 };
 
 handler.help = ['clicker'];
@@ -26,7 +29,5 @@ handler.tags = ['juegos'];
 handler.command = ['clicker', 'click'];
 handler.group = false;
 handler.register = true;
-handler.coin = false;
-handler.limit = false;
 
 export default handler;
