3b5f7135ea7c66c5b3a53f7334e1c1f0abdbf479
Ado <adonaylester85@gmail.com>
2025-04-19 11:08:22 -0600

    Update info-newcommand.js

--- plugins/info-newcommand.js
+++ plugins/info-newcommand.js
@@ -1,15 +1,22 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) return conn.reply(m.chat, `√${emoji} Que comando quieres sugerir?`, m)
-    if (text.length < 10) return conn.reply(m.chat, `${emoji2} La sugerencia debe ser mas de 10 character.`, m)
-    if (text.length > 1000) return conn.reply(m.chat, `${emoji2} Maximo de la sugerencia es de 1000 character.`, m)
-    const teks = `${emoji} Sugerencia de un nuevo comando del usuario *${nombre}*
+    if (!text) return conn.reply(m.chat, `√${emoji} ¿Qué comando quieres sugerir?`, m)
+    if (text.length < 10) return conn.reply(m.chat, `${emoji2} La sugerencia debe tener más de 10 caracteres.`, m)
+    if (text.length > 1000) return conn.reply(m.chat, `${emoji2} El máximo de la sugerencia es de 1000 caracteres.`, m)
+
+    const teks = `${emoji} Sugerencia de un nuevo comando del usuario *${m.pushName || 'Anónimo'}*
 
 ☁️ Comando Sugerido:
 > ${text}`
-    await conn.reply(`${suittag}@s.whatsapp.net`, m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+
+    // Números a los que se enviará la sugerencia
+    const admins = ['50493732693@s.whatsapp.net', '51921826291@s.whatsapp.net']
+    for (let admin of admins) {
+        await conn.reply(admin, m.quoted ? teks + '\n\n' + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+    }
 
     m.reply('🍬 La sugerencia se envió a mi propietario.')
 }
+
 handler.help = ['newcommand']
 handler.tags = ['info']
 handler.command = ['newcommand', 'sug']
