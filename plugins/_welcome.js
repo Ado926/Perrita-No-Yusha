1a35bed660bd67db252cc6575a416c3cdb901516
Ado <adonaylester85@gmail.com>
2025-04-19 11:28:31 -0600

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -6,8 +6,8 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
-  let txt = 'ゲ◜៹ Neww Member :3 ៹◞ゲ'
-  let txt1 = 'ゲ◜៹ Bye Member :c ៹◞ゲ'
+  let txt = '🌸✨ *¡Nuevo miembro! Bienvenid@ a nuestro pequeño paraíso* ✨🌸'
+  let txt1 = '🌙💖 *Adiós, hasta pronto... Te extrañaremos* 💖🌙'
   let groupSize = participants.length
   if (m.messageStubType == 27) {
     groupSize++;
@@ -16,11 +16,12 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `❀ Holiiis :3 *Bienvenid@* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n Gracias a ti. ✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`    
+    let bienvenida = `🌸✨ *¡Bienvenid@ al hermoso grupo de ${groupMetadata.subject}! 🌟* ✨\n💖 *@${m.messageStubParameters[0].split`@`[0]}* ¡Nos alegra mucho que estés aquí! 😍\n🌼 *Gracias por unirte a nuestra pequeña familia.* Ahora somos ${groupSize} miembros, ¡y tú eres una parte muy especial! 🌸\n💌 *No olvides usar #help para explorar todos nuestros comandos y conocer más.*`
     await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
   }
   
   if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = `❀ *Adiós* de ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+    let bye = `🌙💖 *Adiós, pero siempre serás parte de nuestro corazón* 💖🌙\n🌟 *@${m.messageStubParameters[0].split`@`[0]}* ha partido, pero nuestro cariño sigue aquí. 😢💫\n🌸 *Ahora somos ${groupSize} miembros, y siempre te recordaremos con cariño.*\n💌 *Te esperamos con los brazos abiertos, y si quieres regresar, ¡aquí estaremos!*`
     await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak)
-  }}
+  }
+}
